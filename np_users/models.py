9c10cee780002264b4bc844170519d7ddf7fa752
marco <39marcom@gmail.com>
2020-08-20 19:12:47 -0700

    git add np_user

---
+++ np_users/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import ugettext_lazy as _
+
+from .managers import CustomUserManager
+
+
+class CustomUser(AbstractUser):
+    username = None
+    email = models.EmailField(_('email address'), unique=True)
+    npo = models.CharField(_('non profit name'), max_length=200)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
